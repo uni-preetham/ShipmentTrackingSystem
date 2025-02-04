03860ca4e9ca39f18cd7ef1b592f597d528316d7
Preetham A A <preethama@crimsonlogic.com>
2025-02-04 14:34:39 +0530

    spacing and toast

--- Frontend/src/app/components/shipment-form/shipment-form.component.ts
+++ Frontend/src/app/components/shipment-form/shipment-form.component.ts
@@ -10,15 +10,19 @@ import { CheckboxModule } from 'primeng/checkbox';
 import { InputTextModule } from 'primeng/inputtext';
 import { DropdownModule } from 'primeng/dropdown';
 import { FloatLabelModule } from 'primeng/floatlabel';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+
 
 @Component({
   imports: [
     FormsModule, DialogModule, SelectModule, ButtonModule, FloatLabelModule,
-    TableModule, CalendarModule, CheckboxModule, InputTextModule, DropdownModule
+    TableModule, CalendarModule, CheckboxModule, InputTextModule, DropdownModule, ToastModule
   ],
   selector: 'app-shipment-form',
   templateUrl: './shipment-form.component.html',
   styleUrls: ['./shipment-form.component.scss'],
+  providers: [MessageService]
 })
 export class ShipmentFormComponent {
   shipmentDialog: boolean = false;
@@ -49,12 +53,12 @@ export class ShipmentFormComponent {
     notes: ''
   };
 
-  constructor(private shipmentService: ShipmentService) {}
+  constructor(private shipmentService: ShipmentService, private messageService: MessageService) {}
 
   async addShipment() {
     console.log(this.shipment);
     await this.shipmentService.createShipment(this.shipment);
-    alert('Shipment Added Successfully');
+    this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Shipment Added Successfully!' });
     this.shipmentDialog = false;
   }
 
