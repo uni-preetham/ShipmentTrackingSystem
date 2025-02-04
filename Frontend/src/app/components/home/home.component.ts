c759dab999a9f11ffe203f608bc551fca3896444
Preetham A A <preethama@crimsonlogic.com>
2025-02-04 16:05:40 +0530

    homepage

--- Frontend/src/app/components/home/home.component.ts
+++ Frontend/src/app/components/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CardModule, ButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
